4d24d9d889bf2f895a02e11691a8c2a842bc98f5
waihongthong <94752520+waihongthong@users.noreply.github.com>
2025-01-21 22:35:16 +0800

    Update script.py

--- script.py
+++ script.py
@@ -1,38 +1,45 @@
-import pymongo 
-import paho.mqtt.client as mqtt 
-from datetime import datetime  
+import pymongo
+import paho.mqtt.client as mqtt
+from datetime import datetime, timezone
 
-# MongoDB configuration 
-mongo_client = pymongo.MongoClient("mongodb://localhost:27017/") 
-db = mongo_client["smarthome"] 
-collection = db["iot"]  
+# MongoDB configuration
+mongo_client = pymongo.MongoClient("mongodb://localhost:27017/")
+db = mongo_client["smarthome"]
+collection = db["iot"]
 
-# MQTT configuration 
-mqtt_broker_address = '34.143.200.122' 
-mqtt_topic = 'iot'  
+# MQTT configuration
+mqtt_broker_address = "34.68.100.137"
+mqtt_topic = "iot"
 
-def on_message(client, userdata, message):    
-    payload = message.payload.decode('utf-8')     
-    print(f'Received message: {payload}')      
-    
-    # Convert MQTT timestamp to datetime     
-    timestamp = datetime.utcnow()  # Use current UTC time     
-    datetime_obj = timestamp.strftime("%Y-%m-%dT%H:%M:%S.%fZ")      
-    
-    # Process the payload and insert into MongoDB with proper timestamp     
-    document = {
-	'timestamp': datetime_obj,
-	'data': payload
-    }     
-    collection.insert_one(document)     
-    print('Data ingested into MongoDB')  
-client = mqtt.Client()
-client.on_message = on_message  
-    
-# Connect to MQTT broker 
-client.connect(mqtt_broker_address, 1883, 60)  
-    
-# Subscribe to MQTT topic 
-client.subscribe(mqtt_topic)  
-# Start the MQTT loop 
-client.loop_forever() 
+# Define the callback function for connection
+def on_connect(client, userdata, flags, reason_code, properties):
+	if reason_code == 0:
+		print(f"Successfully connected")
+		client.subscribe(mqtt_topic)
+
+# Define the callback function for ingesting data into MongoDB
+def on_message(client, userdata, message):
+	payload = message.payload.decode("utf-8")
+	print(f"Received message: {payload}")
+	
+	# Convert MQTT timestamp to datetime
+	timestamp = datetime.now(timezone.utc)
+	datetime_obj = timestamp.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+	
+	# Process the payload and insert into MongoDB with proper timestamp
+	document = {"timestamp": datetime_obj, "data": payload}
+	collection.insert_one(document)
+	print("Data ingested into MongoDB")
+	
+# Create a MQTT client instance
+client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+
+# Attach the callbacks using explicit methods
+client.on_connect = on_connect
+client.on_message = on_message
+
+# Connect to MQTT broker
+client.connect(mqtt_broker_address, 1883, 60)
+
+# Start the MQTT loop
+client.loop_forever()
